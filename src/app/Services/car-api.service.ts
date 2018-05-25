d31c62cb202062feca32912313e6998bf67df7f5
TNadir <nadir.tariverdiyev@gmail.com>
2018-05-25 08:13:56 +0400

    25.06.2018 http post put

--- src/app/Services/car-api.service.ts
+++ src/app/Services/car-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http'
+import {Http, Response} from '@angular/http'
 
 @Injectable()
 export class CarApiService {
@@ -9,7 +9,32 @@ export class CarApiService {
   getCars()
   {
 
-    return this.http.get('http://localhost:3000/cars');
+    return this.http.get('http://localhost:3000/cars')
+    .map((response:Response)=> response.json());
+
+  }
+
+  addCar(carName:string)
+  {
+
+    const data = {
+
+      name: carName,
+      year: 2018
+
+    };
+
+    return this.http.post('http://localhost:3000/cars',data)
+    .map((response:Response)=> response.json());
+
+  }
+  changeCar(car:any)
+  {
+
+    console.log(car);
+
+    return this.http.put(`http://localhost:3000/cars/${car.id}`,car)
+    .map((response:Response)=> response.json());
 
   }
 
--- src/app/srcapitest/srcapitest.component.ts
+++ src/app/srcapitest/srcapitest.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CarApiService } from '../Services/car-api.service';
 
+
+interface Cars {
+
+  Id: number,
+  name: string,
+  year: number
+
+}
+
 @Component({
   selector: 'app-srcapitest',
   templateUrl: './srcapitest.component.html',
@@ -8,18 +17,46 @@ import { CarApiService } from '../Services/car-api.service';
 })
 export class SrcapitestComponent implements OnInit {
 
-  cars=[];
+  cars: Cars[] = [];
+  carName: string = '';
+
   constructor(private carapi: CarApiService) { }
 
   ngOnInit() {
-  
+
   }
 
   onSubmit() {
-    this.carapi.getCars().subscribe((response) => {
+    this.carapi.getCars().subscribe((cars: Cars[]) => {
 
-      this.cars=response.json();
+      this.cars = cars;
 
     });
   }
+
+  addCar() {
+  
+      
+    this.carapi.addCar(this.carName).subscribe((car: Cars) => {
+
+      this.cars.push(car);
+
+    });
+
+  }
+
+
+updateCar(car:Cars){
+
+car.year=3000;
+
+  this.carapi.changeCar(car).subscribe((res) => {
+
+ //console.log()
+
+  });
+
+}
+
+
 }
